0f53728acd0d4fbe205d9d267c3d6bec389356ca
Mathieu Laurent <mathieu.laurent@gmail.com>
2024-03-11 14:30:03 +0100

    chore: add fontawesome + project cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import { useOruga } from '@oruga-ui/oruga-next'
+
+const oruga = useOruga()
+
+const elementRef = ref<HTMLElement | null>(null)
+
+function openLoading() {
+  oruga.loading.open({
+    fullPage: false,
+    container: elementRef.value
+  })
+}
 </script>
 
 <template>
-  <o-field>
-    <o-button label="Launch loading" size="medium" variant="primary" />
-  </o-field>
+  <section>
+    <o-field>
+      <o-button label="Launch loading" size="medium" variant="primary" @click="openLoading" />
+    </o-field>
 
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <p ref="elementRef" style="position: relative">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin
+      sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+    </p>
+  </section>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import './assets/main.css'
+import '@oruga-ui/theme-bulma/dist/bulma.css'
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss'
+import '@fortawesome/fontawesome-free/scss/regular.scss'
+import '@fortawesome/fontawesome-free/scss/solid.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -6,6 +9,20 @@ import App from './App.vue'
 import Oruga from '@oruga-ui/oruga-next'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
 
-import '@oruga-ui/theme-bulma/dist/bulma.css'
+const app = createApp(App)
 
-createApp(App).use(Oruga, bulmaConfig).mount('#app')
+app.use(Oruga, {
+  ...bulmaConfig,
+  customIconPacks: {
+    fas: {
+      sizes: {
+        default: '',
+        small: 'fa-sm',
+        medium: 'fa-lg',
+        large: 'fa-2x'
+      }
+    }
+  },
+  iconPack: 'fas'
+})
+app.mount('#app')
